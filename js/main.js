6aa0817b8fa5463c1c205153c374982deb403492
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-09-15 20:59:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,68 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 
 
 itens.forEach((elemento) => {
-    criaElementos(elemento)
-})
+    criaElementos(elemento);
+});
 
 
 form.addEventListener("submit", (evento) =>{
-    evento.preventDefault()
+    evento.preventDefault();
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
 
-    const existe = itens.find(elemento => elemento.nome === nome.value)
+    const existe = itens.find(elemento => elemento.nome === nome.value);
        
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
-   }
+   };
 
    if (existe) {
-    itemAtual.id = existe.id
+    itemAtual.id = existe.id;
 
-        atualizaElemento(itemAtual)
+        atualizaElemento(itemAtual);
+
+        itens[existe.id] = itemAtual
 
    } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens.length;
 
-        criaElementos(itemAtual)
+        criaElementos(itemAtual);
 
-        itens.push(itemAtual)
-   }
+        itens.push(itemAtual);
+   };
 
 
-   localStorage.setItem("itens", JSON.stringify(itens))
+   localStorage.setItem("itens", JSON.stringify(itens));
 
-    nome.value = ""
-    quantidade.value = ""
+    nome.value = "";
+    quantidade.value = "";
     
-} )
+} );
 
 function criaElementos(item){
 
-   const novoItem = document.createElement("li")
-   novoItem.classList.add("item")
+   const novoItem = document.createElement("li");
+   novoItem.classList.add("item");
 
-   const numeroItem = document.createElement("strong")
-   numeroItem.innerHTML = item.quantidade
-   numeroItem.dataset.id = item.id
-   novoItem.appendChild(numeroItem)
+   const numeroItem = document.createElement("strong");
+   numeroItem.innerHTML = item.quantidade;
+   numeroItem.dataset.id = item.id;
+   novoItem.appendChild(numeroItem);
 
-   novoItem.innerHTML += item.nome
+   novoItem.innerHTML += item.nome;
 
-   lista.appendChild(novoItem)
+   lista.appendChild(novoItem);
    
 }
 
 function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHtml = item.quantidade
-}
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+};
 
 // localStorage só permite armazenar string, então usamos o metodo JSON.stringify para converter o objeto em string.
